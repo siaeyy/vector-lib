b0d7216cf47473fee975e63c9b312be061648928
siaeyy <iso5374@hotmail.com>
2025-05-23 07:12:27 +0300

    feat: v0.1.1🔖

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,17 @@
 import typescript from 'rollup-plugin-typescript2';
 
+const banner = `
+/*!
+ * @package vector-lib
+ * @version 0.1.1
+ * @date 2025-05-23
+ * @author siaeyy
+ * @license undefined
+ * 
+ * A package for easy usage of vectors.
+ */
+`;
+
 export default {
     input: 'src/index.ts',
     output: [
@@ -7,11 +19,13 @@ export default {
             file: "dist/index.js",
             format: 'cjs',
             sourcemap: true,
+            banner,
         },
         {
             file: "dist/index.mjs",
             format: 'es',
             sourcemap: true,
+            banner,
         },
     ],
     plugins: [
